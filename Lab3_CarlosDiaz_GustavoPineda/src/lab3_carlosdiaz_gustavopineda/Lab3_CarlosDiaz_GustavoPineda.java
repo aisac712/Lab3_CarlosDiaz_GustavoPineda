fc4c4dc046ec0233be25ee696ba4b5789ad60481
aisac712 <isaac.diaz712gmail.com>
2020-08-07 13:48:20 -0600

    creacion de clases

---
+++ Lab3_CarlosDiaz_GustavoPineda/src/lab3_carlosdiaz_gustavopineda/Lab3_CarlosDiaz_GustavoPineda.java
@@ -0,0 +1,12 @@
+package lab3_carlosdiaz_gustavopineda;
+
+import java.util.Scanner;
+
+public class Lab3_CarlosDiaz_GustavoPineda {
+    static Scanner leer = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
